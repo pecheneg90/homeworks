1cf7e24abe439a68204ff0ba1443f0f5207f90b2
Vadim <manjavha@gmail.com>
2022-03-29 23:48:28 +0300

    Initial commit

---
+++ src/main/java/com/example/homeworks/HomeworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homeworks/HomeworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homeworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
