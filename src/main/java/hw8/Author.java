b326b14fe834ad76ee212e8bd0a05c8264e41f70
Vadim <manjavha@gmail.com>
2022-03-30 04:17:41 +0300

    hw8 Classes and Objects done!

--- src/main/java/hw8/Author.java
+++ src/main/java/hw8/Author.java
@@ -1,4 +1,25 @@
 package hw8;
 
 public class Author {
+    private final String firstName;
+    private final String lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return getFirstName() + " " + getLastName();
+    }
 }
--- src/main/java/hw8/Main.java
+++ src/main/java/hw8/Main.java
@@ -1,4 +1,13 @@
 package hw8;
 
 public class Main {
+    public static void main(String[] args) {
+        Book book1 = new Book("War and Peace", new Author("Lev", "Tolstoy"), 1869);
+        book1.setYearPublication(2021);
+        System.out.println("название книги: " + book1.getTitle() + ", автор: " + book1.getNameAuthor() + ", год издания: " + book1.getYearPublication());
+
+        Book book2 = new Book("The Stand", new Author("Stephen", "King"), 1990);
+        book2.setYearPublication(2012);
+        System.out.println("название книги: " + book2.getTitle() + ", автор: " + book2.getNameAuthor() + ", год издания: " + book2.getYearPublication());
+    }
 }
