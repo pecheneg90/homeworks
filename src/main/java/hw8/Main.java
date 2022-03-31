2ed736f29054fa06ca22e6f0169869d25570a48c
Vadim <manjavha@gmail.com>
2022-04-01 00:36:06 +0300

    first commit hw9 Classes and Objects (toString, equals, hashcode

--- src/main/java/hw8/Main.java
+++ src/main/java/hw8/Main.java
@@ -3,7 +3,7 @@ package hw8;
 public class Main {
     public static void main(String[] args) {
         Book book1 = new Book("War and Peace", new Author("Lev", "Tolstoy"), 1869);
-        book1.setYearPublication(2021);
+        book1.setYearPublication(2023);
         System.out.println("название книги: " + book1.getTitle() + ", автор: " + book1.getNameAuthor() + ", год издания: " + book1.getYearPublication());
 
         Book book2 = new Book("The Stand", new Author("Stephen", "King"), 1990);
